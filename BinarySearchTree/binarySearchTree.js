829c64c5bddfcf83d3b629941ac77bbe7c651809
sumit kumar <sumit-kumar@sumit-kumar.local>
2020-09-23 16:09:57 +0530

    added remove

--- BinarySearchTree/binarySearchTree.js
+++ BinarySearchTree/binarySearchTree.js
@@ -1,3 +1,6 @@
+/**
+ * A binary search tree is made of nodes which contain a value and pointers to the left and the right children
+ */
 class Node {
   constructor(value) {
     this.value = value;
@@ -6,14 +9,33 @@ class Node {
   }
 }
 
+/**
+ * Binary Search tree basic property is its root. We can perform any operation using this root.
+ *
+ *
+ *                  11
+ *          8---------------12
+ *      6-------9               13
+ *  4-------7       10              14
+ *      5
+ *
+ */
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
   }
 
+  /**
+   *
+   * @param {number} value
+   */
   insertNode(value) {
     var node = new Node(value);
     var current = this.root;
+    if (this.findNode(value)) {
+      return;
+    }
     if (!this.root) {
       this.root = node;
     } else {
@@ -45,7 +67,7 @@ class BinarySearchTree {
     var current = this.root;
     while (current) {
       if (value === current.value) {
-        return true;
+        return current;
       }
       if (value < current.value) {
         current = current.left;
@@ -66,6 +88,7 @@ class BinarySearchTree {
     })(current);
     return values;
   }
+
   postOrderTraversal() {
     const values = [];
     var current = this.root;
@@ -76,6 +99,7 @@ class BinarySearchTree {
     })(current);
     return values;
   }
+
   inOrderTraversal() {
     const values = [];
     var current = this.root;
@@ -86,6 +110,7 @@ class BinarySearchTree {
     })(current);
     return values;
   }
+
   BFSTraversal() {
     var queue = [];
     var data = [];
@@ -99,11 +124,116 @@ class BinarySearchTree {
     return data;
   }
 
-  deleteNode() {}
+  getMinValue(root) {
+    if (!root) {
+      return null;
+    }
+    var current = root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+  getMaxValue(root) {
+    if (!root) {
+      return null;
+    }
+    var current = root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current;
+  }
+
+  inorderSuccessor(value) {
+    const node = this.findNode(value);
+
+    if (node) {
+      if (node.right !== null) {
+        return this.getMinValue(node.right);
+      } else {
+      }
+    }
+  }
+
+  inorderPredecessor(value) {
+    const node = this.findNode(value);
+    if (node) {
+      if (node.left !== null) {
+        return this.getMaxValue(node.left);
+      }
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+  // a recursive function to insert a new value in binary search tree
+  removeNode(current, value) {
+    if (current === null || !this.findNode(value)) return current;
+    if (value === current.value) {
+      if (current.left === null && current.right === null) {
+        return null;
+      } else if (current.left === null) {
+        return current.right;
+      } else if (current.right === null) {
+        return current.left;
+      } else {
+        let tempNode = this.inorderSuccessor(current.right.value);
+        current.value = tempNode.value;
+        current.right = this.removeNode(current.right, tempNode.value);
+        return current;
+      }
+    } else if (value < current.value) {
+      current.left = this.removeNode(current.left, value);
+      return current;
+    } else {
+      current.right = this.removeNode(current.right, value);
+      return current;
+    }
+  }
+
+  getParent(target) {
+    if (!this.root || !this.findNode(target) || target === this.root.value) {
+      return null;
+    }
+    var current = this.root;
+    function helper(current) {
+      if (
+        (current.left && current.left.value === target) ||
+        (current.right && current.right.value === target)
+      ) {
+        return current;
+      }
+      if (target < current.value) {
+        return helper(current.left);
+      } else {
+        return helper(current.right);
+      }
+    }
+    return helper(current);
+  }
 }
 
 var bst = new BinarySearchTree();
-bst.insertNode(10);
-bst.insertNode(7);
+bst.insertNode(11);
+bst.insertNode(8);
 bst.insertNode(12);
+bst.insertNode(6);
+bst.insertNode(9);
+bst.insertNode(10);
+bst.insertNode(13);
+bst.insertNode(4);
+bst.insertNode(5);
+bst.insertNode(7);
 bst.insertNode(14);
+
+// console.log(bst.BFSTraversal());
+// console.log(bst.deleteNode(51));
+// console.log(bst.deleteNode(51));
+// console.log(bst.getMinValue(bst.root));
+// console.log(bst.getMaxValue(bst.root));
+// console.log(bst.inorderPredecessor(12));
+console.log(bst.preOrderTraversal());
+console.log(bst.remove(8));
+console.log(bst.preOrderTraversal());
